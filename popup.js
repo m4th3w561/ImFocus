541c14443dff032a920cde8ce062dbdac9dcb4aa
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2022-10-25 21:07:14 +0900

    init

---
+++ popup.js
@@ -0,0 +1,9 @@
+console.log('content running')
+const block = document.getElementById('block')
+const unblock = document.getElementById('unblock')
+
+block.addEventListener('click',()=> console.log('block'))
+unblock.addEventListener('click',()=> console.log('unblock'))
+
+// window.url
+// figure out how to send message(url) to backgorund
